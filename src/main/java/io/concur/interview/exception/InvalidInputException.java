761d55795d3b824d339a479205fff231609b44aa
aseem80 <aseem@tgtiio.com>
2017-04-14 12:19:27 -0700

    Completed assignment

---
+++ src/main/java/io/concur/interview/exception/InvalidInputException.java
@@ -0,0 +1,11 @@
+package io.concur.interview.exception;
+
+/**
+ * Created by aseem80 on 4/14/17.
+ */
+public class InvalidInputException extends RuntimeException {
+
+    public InvalidInputException(String message) {
+        super( message );
+    }
+}
